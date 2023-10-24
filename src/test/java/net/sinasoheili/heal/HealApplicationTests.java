f5849da7747834b6d052eea5f0c2f05be8eb8304
Sina <sinasoheili79@gmail.com>
2023-10-24 07:25:01 +0330

    Start Project

---
+++ src/test/java/net/sinasoheili/heal/HealApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sinasoheili.heal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
