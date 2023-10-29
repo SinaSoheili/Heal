27bce3914ead2fa421cddbe6b808541aee639968
Sina <sinasoheili79@gmail.com>
2023-10-29 08:03:35 +0330

    Create UserEntity to model user table

---
+++ src/main/java/net/sinasoheili/heal/user/Gender.java
@@ -0,0 +1,5 @@
+package net.sinasoheili.heal.user;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/net/sinasoheili/heal/user/UserEntity.java
@@ -0,0 +1,26 @@
+package net.sinasoheili.heal.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private float height;
+}
