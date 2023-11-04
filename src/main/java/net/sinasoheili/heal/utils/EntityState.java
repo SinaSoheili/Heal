fd791602bbb116a7ca4d26726bd6cfe40e02b591
Sina <sinasoheili79@gmail.com>
2023-11-04 08:10:17 +0330

    Create marker interface to validate user input in controller layer

---
+++ src/main/java/net/sinasoheili/heal/utils/EntityState.java
@@ -0,0 +1,9 @@
+package net.sinasoheili.heal.utils;
+
+/**
+ * A marker interface used to bean validation to define create or update entity
+ */
+public interface EntityState {
+    interface Create {}
+    interface Update {}
+}
