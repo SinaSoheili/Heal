8d664de6e2b4877139d32f7c12cd4701cba38094
Sina <sinasoheili79@gmail.com>
2023-11-08 07:13:54 +0330

    remove jax-rs

--- src/main/java/net/sinasoheili/heal/HealApplication.java
+++ src/main/java/net/sinasoheili/heal/HealApplication.java
@@ -1,17 +1,10 @@
 package net.sinasoheili.heal;
 
-import jakarta.ws.rs.ApplicationPath;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ApplicationPath("/api")
-public class HealApplication extends ResourceConfig {
-
-	public HealApplication() {
-		packages("net.sinasoheili.heal");
-	}
+public class HealApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealApplication.class, args);
