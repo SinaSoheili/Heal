bedf57de97e2dc5ab2c88dd5c1d57ceb79104dd0
Sina <sinasoheili79@gmail.com>
2023-12-26 07:02:19 +0330

    implement UserDetails interface

--- src/main/java/net/sinasoheili/heal/user/UserEntity.java
+++ src/main/java/net/sinasoheili/heal/user/UserEntity.java
@@ -3,16 +3,23 @@ package net.sinasoheili.heal.user;
 import jakarta.persistence.*;
 import lombok.Data;
 import net.sinasoheili.heal.drug.DrugEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
 @Table(name = "user")
 @Data
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
 
     @Column(nullable = false)
     private String firstName;
@@ -22,6 +29,9 @@ public class UserEntity {
 
     @Column(nullable = false, unique = true)
     private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -35,4 +45,39 @@ public class UserEntity {
 
     @OneToMany(mappedBy = DrugEntity.USER_PROP)
     private List<DrugEntity> drugs;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.EMPTY_LIST;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
